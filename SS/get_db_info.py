181f6aca00b09b33f82a583e57719719e78e4d2a
trivi9cri <trivi9cri@gmail.com>
2016-09-26 18:00:22 +0900

    add hex string to ascii string

--- SS/get_db_info.py
+++ SS/get_db_info.py
@@ -2,8 +2,11 @@ from sqlplus_commando import SqlplusCommando
 from subprocess import *
 from pandas import Series, DataFrame
 import pandas as pd
+import re
+import sys
+import binascii
 
-db = 'sample db'
+db = 'mos_view/mos_view@106.240.225.99:1521/orcl'
 
 
 def sql_connection(conn, sql):
@@ -18,6 +21,7 @@ def get_table_info():
 	tb_txt = open('tables.txt', 'r')
 	tb_file =  'table_info.txt'
 	table_info = open(tb_file, 'w')
+	
 	while 1:
 		line = tb_txt.readline()
 		if not line: break
@@ -26,6 +30,7 @@ def get_table_info():
 			temp.remove('')
 		str_tmp = ''.join(str(tmp+' ') for tmp in temp)
 		table_info.write(str_tmp+'\n')
+	
 	tb_txt.close()
 	table_info.close()
 	return tb_file
@@ -35,6 +40,7 @@ def make_TbInfo_df(filename):
 	table_file = open(filename, 'r')
 	owners = []
 	tb_names = []
+
 	while 1:
 		line = table_file.readline()
 		if not line: break
@@ -45,16 +51,51 @@ def make_TbInfo_df(filename):
 				tb_names.append(temp[1])
 	data = { owners.pop(0): owners, tb_names.pop(0) :tb_names}
 	table_data = pd.DataFrame(data)
-	print table_data
+	#print table_data
 	return table_data
 
 
+def hex_to_str(hexstr):
+	hexstr_n = hexstr.split('\\n')
+	hex_n_list = []
+	for hexn in hexstr_n:
+		hexstr_t = hexn.split('\\t')
+		hex_n_list += hexstr_t
+	print hex_n_list
+	# # hexstrs = re.split(r'([ ,.?\'],[\\n])', hexstr)
+	# resstr = ''
+	# for hexs in hexstrs:
+	# 	if '\\x' in hexs:
+	# 		hexs = re.sub(r"[\\x]","",hexs)
+	# 		hexs = hexs.strip()
+			
+	# 		strtmp = binascii.unhexlify(hexs)
+	# 		resstr+= strtmp
+	# 	else: resstr+= hexs
+	# return resstr
+
 
 def get_column_info():
-	make_TbInfo_df(get_table_info())
-	#column_query = 'DESC %s;' % (table_name)
-	#print sql_connection(db, column_query)
+	table_data = pd.DataFrame()
+	table_data = make_TbInfo_df(get_table_info())
+	strtmp = ''
 
+	#tb_df_r = table_data.shape[0]
+	#tb_df_c = table_data.shape[1]
+
+	tables = table_data[table_data.columns[0]]
+	i = 0
+	for table_name in tables:
+		column_query = 'DESC %s;' % (table_name)
+		qur_res = sql_connection(db, column_query)
+		if '\\x' in str(qur_res):
+			strtmp =hex_to_str(str(qur_res))
+		# 	print strtmp
+		# else: print qur_res
+
+		i+=1
+		if i == 10:
+			break
 
 
 def main():
@@ -63,5 +104,4 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
 
