885c247539927d5b55056cc3f6b2cbc8788acebd
trivi9cri <trivi9cri@gmail.com>
2016-11-18 16:59:33 +0900

    Weekly and Daily Graph

--- SS/DataPrePro.py
+++ SS/DataPrePro.py
@@ -8,15 +8,14 @@ import numpy
 import math
 import matplotlib.pyplot as plt
 import matplotlib.legend as legend
-
-
+import datetime
 
 class PreProcess():
 
    def makeDF(self):
-      user = 'USER'
-      pw = 'USER'
-      dbenv = 'DB'
+      user = ''
+      pw = ''
+      dbenv = ''
       conn = odb.connect(user + '/' + pw + '@' + dbenv)
       sqlF = open('/home/trivi9ri/2016KETI/SS/LineUPH3.sql')
       LineSql = sqlF.read()
@@ -96,6 +95,7 @@ class PreProcess():
       LL3 = []
       LL2 = []
       LL = []
+      UCL = []
       total_Ri = 0
       Ri.append(0)
       Rmean.append(0)
@@ -120,12 +120,16 @@ class PreProcess():
         
       for n in range(row_cnt):
           Sd.append(Spi[n]*Sz[row_cnt-1])
-          UL3.append(Pmean[row_cnt-1]+3 * Sd[n])
+          if Pmean[row_cnt-1]+3 * Sd[n] > 1:
+             UL3.append(1.0)
+          else:
+             UL3.append(Pmean[row_cnt-1]+3 * Sd[n])
           UL2.append(Pmean[row_cnt-1]+2 * Sd[n])
           UL.append(Pmean[row_cnt-1]+Sd[n])
           LL3.append(Pmean[row_cnt-1]-3*Sd[n])
           LL2.append(Pmean[row_cnt-1]-2*Sd[n])
           LL.append(Pmean[row_cnt-1]-Sd[n])
+          UCL.append(1.0)
         
       DataF['Pi'] = Pi
       DataF['Pmean'] = Pmean
@@ -136,44 +140,105 @@ class PreProcess():
       DataF['Sz'] = Sz
       DataF['Sd'] = Sd
       DataF['CL'] = Pmean
-      DataF['3UL'] = UL3
+      if RateType == 'TIME':
+         DataF['3UL'] = UCL
+      elif RateType == 'PERF':
+         DataF['3UL'] = UL3
       DataF['2UL'] = UL2
       DataF['UL'] = UL
       DataF['3LL'] = LL3
       DataF['2LL'] = LL2
       DataF['LL'] = LL
-    
-      self.drawGraph(DataF,RateType)        
+   
+
+      self.DailyDrawGraph(DataF,RateType)
+      self.WeeklyDrawGraph(DataF,RateType)        
  #     DataF.to_csv('example.csv')i
-   def drawGraph(self,df,RateType):
+
+   def DailyDrawGraph(self,df,RateType):
       print RateType
       LocArr = df.LOCATION_ID.unique()
       for a in LocArr:
          df_LN = df[df['LOCATION_ID'] == a]
          LNPi = df_LN['Pi'].tolist()
          LNCL = df_LN['CL'].tolist()
-         LN3UL = df_LN['3UL'].tolist()
+         LNUB = df_LN['3UL'].tolist()
          LN3LL = df_LN['3LL'].tolist()
          ax = []
+         min_y = min(LN3LL)
          for x in df_LN['REQUEST_TIME']:
             ax.append(x[5:])
          x = range(len(ax)) 
          plt.xticks(x,ax)
          plt.xticks(rotation=60)
-         plt.plot(x, LNPi,x, LNCL,x,LN3UL,x,LN3LL)
-         plt.title(a)
+         plt.ylim(min_y,1.2)
+         plt.plot(x, LNPi,x, LNCL,x,LNUB,x,LN3LL, marker = 'o')
+         plt.title("Laney P' Chart Daily"+a)
          plt.xlabel('Date')
          plt.ylabel('Proportion')
-         plt.legend(['Pi','CL','3UL','3LL'],loc='best')
+         plt.legend(['Pi','CL','UB','3LL'],loc='best')
          if RateType == 'TIME':
       #      print "tt"
-            plt.savefig("/home/trivi9ri/2016KETI/SS/T"+a+".png",dpi = 72)
+            plt.savefig("/home/trivi9ri/2016KETI/SS/T_Daily"+a+".png",dpi = 72)
          elif RateType == 'PERF':
       #      print "pp"
-            plt.savefig("/home/trivi9ri/2016KETI/SS/P"+a+".png",dpi = 72)
+            plt.savefig("/home/trivi9ri/2016KETI/SS/P_Daily"+a+".png",dpi = 72)
          plt.cla()
-         plt.clf()      
+         plt.clf()
 
+   def WeeklyDrawGraph(self,df,RateType):
+      LocArr = df.LOCATION_ID.unique()
+      for a in LocArr:
+         DfWeek = df[df['LOCATION_ID'] == a ] 
+         row_cnt = DfWeek.shape[0]
+         x = 0
+         idx = 1
+         LNPi = []
+         LNCL = []
+         LN3LL = []
+         LNUB = []
+         ax = []
+         while( x < row_cnt):
+            reqDay = datetime.datetime.strptime(DfWeek['REQUEST_TIME'].iloc[x],'%Y-%m-%d')
+            dura = datetime.timedelta(days=7)
+            enDay = reqDay + dura
+            cnt = 0
+            sum_pi = 0
+            sum_cl = 0
+            sum_3ll = 0
+            sum_ub = 0
+            min_y = min(DfWeek['3LL'])
+            while((reqDay < enDay) & (x < row_cnt)):
+               cnt += 1.0
+               sum_pi += DfWeek['Pi'].iloc[x]
+               sum_cl += DfWeek['CL'].iloc[x]
+               sum_3ll += DfWeek['3LL'].iloc[x]
+               sum_ub += DfWeek['3UL'].iloc[x]
+               x += 1
+               if x<row_cnt:
+                  reqDay = datetime.datetime.strptime(DfWeek['REQUEST_TIME'].iloc[x],'%Y-%m-%d')
+            LNPi.append(float(sum_pi/cnt))
+            LNCL.append(float(sum_cl/cnt))
+            LNUB.append(float(sum_ub/cnt))
+            LN3LL.append(float(sum_3ll/cnt))
+            ax.append('WEEK %d'%idx)
+            idx += 1
+         len_x = range(len(ax))
+         plt.xticks(len_x,ax)
+         plt.xticks(rotation = 60)
+         plt.ylim(min_y,1.2)
+         plt.plot(len_x, LNPi, len_x, LNCL, len_x, LNUB, len_x, LN3LL, marker = 'o')
+         plt.title("Laney P' Chart Weekly "+a)
+         plt.xlabel('Week')
+         plt.ylabel('Proportion')
+         plt.legend(['Pi','CL','UB','3LL'], loc='best')
+         if RateType == 'TIME':
+            plt.savefig("/home/trivi9ri/2016KETI/SS/T_Weekly"+a+".png", dpi = 72)
+         elif RateType == 'PERF':
+            plt.savefig("/home/trivi9ri/2016KETI/SS/P_Weekly"+a+".png", dpi = 72)
+         plt.cla()
+         plt.clf()
+      
 if __name__ == '__main__':
    dpp = PreProcess()
    dpp.DataPre('TIME')
